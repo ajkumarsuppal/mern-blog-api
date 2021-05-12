ef9255c771566bba375c1cca7b9e06d57e3a0ea3
Ajay Suppal <akrsuppal@gmail.com>
2021-05-13 02:14:50 +0530

    required the route files and used them

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,10 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully')
 })
 
+const blogRouter = require('./routes/blogs')
+
+app.use('/blogs', blogRouter)
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
 })
