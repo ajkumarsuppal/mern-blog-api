452497fbc688c29abce37efc0389625a28b2c006
Ajay Suppal <akrsuppal@gmail.com>
2021-05-13 22:43:04 +0530

    destructed code, added findbyid

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,13 +4,22 @@ let Blog = require('../models/blog')
 router.route('/').get((req, res) => {
   Blog.find()
     .then((blogs) => res.json(blogs))
-    .catch((err) => res.status(400).json('Error: ' + err))
+    .catch((err) =>
+      res.status(400).json('Error while fetching all the blogs: ' + err)
+    )
+})
+
+router.get('/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      if (blog) return res.status(200).json({ blog, msg: 'Blog found!!' })
+      else return res.status(200).json('No data found!! ')
+    })
+    .catch((err) => res.status(400).json('Error while fetching a blog: ' + err))
 })
 
 router.route('/add').post((req, res) => {
-  const title = req.body.title
-  const author = req.body.author
-  const content = req.body.content
+  const { title, author, content } = req.body
 
   const newBlog = new Blog({
     title,
@@ -19,7 +28,7 @@ router.route('/add').post((req, res) => {
   })
   newBlog
     .save()
-    .then(() => res.json('Blog added!'))
+    .then((blog) => res.status(200).json({ blog, msg: 'Blog added!!' }))
     .catch((err) => res.status(400).json('Error: ' + err))
 })
 
