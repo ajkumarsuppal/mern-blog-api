0a66857432ac55c975d69fb57de8a838d22c0664
Ajay Suppal <akrsuppal@gmail.com>
2021-05-15 13:30:13 +0530

    add and test update blog route

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 let Blog = require('../models/blog')
 
+//get all blogs
 router.route('/').get((req, res) => {
   Blog.find()
     .then((blogs) => res.json(blogs))
@@ -9,6 +10,7 @@ router.route('/').get((req, res) => {
     )
 })
 
+//get a blog by id
 router.get('/:id', (req, res) => {
   Blog.findById(req.params.id)
     .then((blog) => {
@@ -18,6 +20,7 @@ router.get('/:id', (req, res) => {
     .catch((err) => res.status(400).json('Error while fetching a blog: ' + err))
 })
 
+//add a blog
 router.route('/add').post((req, res) => {
   const { title, author, content } = req.body
 
@@ -30,6 +33,24 @@ router.route('/add').post((req, res) => {
     .save()
     .then((blog) => res.status(200).json({ blog, msg: 'Blog added!!' }))
     .catch((err) => res.status(400).json('Error: ' + err))
+})
+
+//edit a blog by id
+router.post('/update/:id', (req, res) => {
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      blog.title = req.body.title
+      blog.author = req.body.author
+      blog.content = req.body.content
+
+      blog
+        .save()
+        .then((blog) => res.status(200).json({ blog, msg: 'Blog updated!!' }))
+        .catch((err) =>
+          res.status(400).json('Blog could not be updated due to ' + err)
+        )
+    })
+    .catch((err) => res.status(200).json('Error:' + err))
 })
 
 module.exports = router
